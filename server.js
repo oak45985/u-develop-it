8538d7a0a74aad4ced66fe34ebbe71250c1eed43
oak45985 <oak45985@gmail.com>
2022-08-14 16:22:06 -0500

    updates

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const mysql = require('mysql2');
 const express = require('express');
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -5,7 +6,16 @@ const app = express();
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-
+// Connect to Database
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        user: 'root',
+        password: 'J0hnny@non',
+        database: 'election'
+    },
+    console.log('Connected to the election database.')
+);
 
 app.use((req, res) => {
     res.status(404).end();
