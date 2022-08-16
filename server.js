b3aa23b984e058312b1c353cd0191d3e4b80990e
oak45985 <oak45985@gmail.com>
2022-08-15 22:04:47 -0500

    updates

--- server.js
+++ server.js
@@ -96,18 +96,6 @@ app.post('/api/candidate', ({ body }, res) => {
         });
     });
 });
-// Create a candidate
-// const sql = `INSERT INTO candidates (id, first_name, last_name, industry_connected)
-//     VALUES (?,?,?,?)`;
-
-// const params = [1, 'Ronald', 'Firbank', 1];
-
-// db.query(sql, params, (err, result) => {
-//     if (err) {
-//         console.log(err);
-//     }
-//     console.log(result);
-// });
 
 app.use((req, res) => {
     res.status(404).end();
