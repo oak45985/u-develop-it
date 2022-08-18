96376272f602a7de713aa23d0c8db793f7447805
oak45985 <oak45985@gmail.com>
2022-08-18 10:27:04 -0500

    updated feature/votes

--- routes/apiRoutes/voteRoutes.js
+++ routes/apiRoutes/voteRoutes.js
@@ -3,6 +3,29 @@ const router = express.Router();
 const db = require('../../db/connection');
 const inputCheck = require('../../utils/inputCheck');
 
+//get total votes for all candidates
+router.get('/votes', (req, res) => {
+    const sql = `SELECT candidates.*, parties.name AS party_name,
+        COUNT (candidate_id)
+        AS count FROM votes
+        LEFT JOIN candidates ON votes.candidate_id = candidates.id
+        LEFT JOIN parties ON candidates.party_id = parties.id
+        GROUP BY candidate_id
+        ORDER BY count DESC`;
+
+    db.query(sql, (err, rows) => {
+        if(err) {
+            res.status(500).json({ error: err.message });
+            return;
+        }
+        res.json({
+            message: 'success',
+            data: rows
+        });
+    });
+});
+
+//create a vote record 
 router.post('/vote', ({ body }, res) => {
     const errors = inputCheck(body, 'voter_id', 'candidate_id');
     if (errors) {
